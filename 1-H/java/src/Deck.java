c255391eaf4a34dce973bd7af8d2e5b98792d79b
Meitaiyang <shrek473@gmail.com>
2023-11-30 23:42:05 +0800

    Delete old Version

--- 1-H/java/src/Deck.java
+++
@@ -1,6 +0,0 @@
-public class Deck {
-
-    public void shuffle() {
-    }
-    
-}
--- 1-H/java/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Showdown game = new Showdown();
-        game.start();
-    }
-}
