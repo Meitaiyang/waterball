69c56eb51aa538d4cb693f98213726654671f77b
Meitaiyang <shrek473@gmail.com>
2023-12-04 23:54:42 +0800

    start coding UNO Java

---
+++ 2.2.H/UNO/src/Player.java
@@ -0,0 +1,14 @@
+public abstract class Player {
+    private String name;
+    private int points;
+    private Hand hand;
+
+    public abstract String nameSelf();
+
+    public void showCard(){
+
+    }
+
+    
+
+}
